f7d7d677b9db00dd94d494a5fb825edac6c99e16
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-09-05 00:09:13 +0300

    task 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.getDuration().then(function (duration) {
+  console.log('duration in seconds:', duration);
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+function onTimeUpdate({ seconds }) {
+  const savedTime = seconds;
+  console.log(savedTime);
+  localStorage.setItem('videoplayer-current-time', savedTime);
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime);
