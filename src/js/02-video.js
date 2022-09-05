e35c57914301cf2cca4679afae828c0c24fe8c1a
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-09-05 12:52:32 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,10 +8,17 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
+function convertTime(value) {
+  let time = '';
+  const minutes = Math.floor(value / 60);
+  const seconds = Math.floor(value % 60);
+  return seconds < 10
+    ? (time = `${minutes}:0${seconds}`)
+    : (time = `${minutes}:${seconds}`);
+}
+
 player.getDuration().then(function (duration) {
-  console.log(
-    `Video duration: ${Math.floor(duration / 60)}:${Math.floor(duration % 60)}`
-  );
+  console.log('Video duration: ' + convertTime(duration));
 });
 
 player.on('play', function () {
@@ -21,17 +28,13 @@ player.on('play', function () {
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
 function onTimeUpdate({ seconds }) {
   const savedTime = seconds;
-  console.log(savedTime);
+  console.log(convertTime(savedTime));
   localStorage.setItem('videoplayer-current-time', savedTime);
 }
 
 const currentTime = localStorage.getItem('videoplayer-current-time');
 if (currentTime) {
   player.setCurrentTime(currentTime).then(function (duration) {
-    console.log(
-      `Th video resumed at ${Math.floor(duration / 60)}:${Math.floor(
-        duration % 60
-      )}`
-    );
+    console.log('The video resumed at ' + convertTime(duration));
   });
 }
